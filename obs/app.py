16c81d644474c8e129fcc36193963f5316dcc632
Muhammet Öztürk <180087422+kaiser-dot-exe@users.noreply.github.com>
2025-06-02 06:53:45 +0300

    refactor

--- obs/app.py
+++ obs/app.py
@@ -11,74 +11,74 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
 @app.route('/')
 def hello_world():
-    if "username" in session:
-        return render_template("index.html", username=session["username"])
-    return redirect(url_for("login"))
+    if 'username' in session:
+        return render_template('index.html', username=session['username'])
+    return redirect(url_for('login'))
 
 
 @app.route('/viewAkademik')
 def viewAkademik():
-    con = sqlite3.connect("ogrenciler.sqlite")
+    con = sqlite3.connect('ogrenciler.sqlite')
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    cur.execute("SELECT ad, soyad, fakulte, bolum FROM akademik_kadro")
+    cur.execute('SELECT ad, soyad, fakulte, bolum FROM akademik_kadro')
     rows = cur.fetchall()
-    return render_template("view.html", rows=rows)
+    return render_template('view.html', rows=rows)
 
 
 @app.route('/viewOgrenciler')
 def viewOgrenciler():
-    con = sqlite3.connect("ogrenciler.sqlite")
+    con = sqlite3.connect('ogrenciler.sqlite')
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    cur.execute("SELECT * FROM ogrenciler WHERE sinif=2")
+    cur.execute('SELECT * FROM ogrenciler WHERE sinif=2')
     rows = cur.fetchall()
-    return render_template("viewOgr.html", rows=rows)
+    return render_template('viewOgr.html', rows=rows)
 
 
 # ZAFİYET OLAN
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
-        username = request.form["username"]
-        numara = request.form["numara"]
+    if request.method == 'POST':
+        username = request.form['username']
+        numara = request.form['numara']
 
-        con = sqlite3.connect("ogrenciler.sqlite")
+        con = sqlite3.connect('ogrenciler.sqlite')
         cur = con.cursor()
         query = f"SELECT * FROM ogrenciler WHERE ad = '{username}' AND numara = '{numara}'"
         cur.execute(query)
         user = cur.fetchone()
 
         if user:
-            session["username"] = username
-            return redirect(url_for("hello_world"))
+            session['username'] = username
+            return redirect(url_for('hello_world'))
         else:
-            return render_template("login.html", error="Kullanıcı bulunamadı!")
+            return render_template('login.html', error="Kullanıcı bulunamadı!")
 
-    return render_template("login.html")
+    return render_template('login.html')
 
-
- #ZAFİYET OLMAYAN
-# @app.route("/login", methods=["GET", "POST"])
-# def login():
-#     if request.method == "POST":
-#         username = request.form["username"]
-#         numara = request.form["numara"]
 #
-#         con = sqlite3.connect("ogrenciler.sqlite")
+#  ZAFİYET OLMAYAN
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         username = request.form['username']
+#         numara = request.form['numara']
+#
+#         con = sqlite3.connect('ogrenciler.sqlite')
 #         cur = con.cursor()
-#         cur.execute("SELECT * FROM ogrenciler WHERE ad = ? AND numara = ?", (username, numara))
+#         cur.execute('SELECT * FROM ogrenciler WHERE ad = ? AND numara = ?', (username, numara))
 #         user = cur.fetchone()
 #
 #         if user:
-#             session["username"] = username
-#             return redirect(url_for("hello_world"))
+#             session['username'] = username
+#             return redirect(url_for('hello_world'))
 #         else:
-#             return render_template("login.html", error="Kullanıcı bulunamadı!")
+#             return render_template('login.html', error='Kullanıcı bulunamadı!')
 #
-#     return render_template("login.html")
-
-
+#     return render_template('login.html')
+#
+#
 
 
 
